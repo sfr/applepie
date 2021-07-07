ac9d3610385c05b7cac85b849a25a169ced49ff2
Juraj Šofranko <juraj.sofranko@gmail.com>
2021-07-07 10:05:02 +0200

    before vcr-error test

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -1,4 +1,13 @@
 test_that("get_packages() works", {
-  pkgs <- get_packages("maelle")
+  vcr::use_cassette("packages", {
+    pkgs <- get_packages("maelle")
+  })
   expect_vector(pkgs, character())
 })
+
+# test_that("get_packages() error", {
+#   vcr::skip_if_vcr_off()
+#   vcr::use_cassette("packages-error", {
+#     expect_error(get_packages("maelle"), "take off")
+#   })
+# })
